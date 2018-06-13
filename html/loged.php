30f88bf6bbca37b8afb45b8b3ef12086877170e6
Ben <534541867@qq.COM>
2018-06-13 22:30:52 +0800

    session&CAPTCHA

---
+++ html/loged.php
@@ -0,0 +1,149 @@
+<?php
+session_start();
+$name = $_SESSION['username'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>莹家 - 走遍天下，落脚此家</title>
+    <link rel="shortcut icon" href="../images/LOGO-label.ico">
+    <link rel="stylesheet" href="../css/common.css" type="text/css">
+    <link rel="stylesheet" href="../css/index.css" type="text/css">
+    <link href="../js/mf-pattern/mF_kdui.css" rel="stylesheet" type="text/css">
+    <script src="../js/myfocus-2.0.4.full.js" type="text/javascript"></script>
+    <script src="../js/mf-pattern/mF_kdui.js" type="text/javascript"></script>
+    <script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="../js/index.js"></script>
+</head>
+<body>
+<div class="index-header">
+    <div class="index-header-banner" id="header-banner">
+        <div class="loading"><img src="../js/mf-pattern/img/loading.gif" alt="图片加载中..."></div>
+        <div class="pic">
+            <ul>
+                <li><img src="../images/search01.jpg"></li>
+                <li><img src="../images/search02.jpg"></li>
+                <li><img src="../images/search03.jpg"></li>
+                <li><img src="../images/search04.jpg"></li>
+            </ul>
+        </div>
+        <div class="index-header-cover"></div>
+        <div class="index-header-nav">
+            <a href=""><img class="logo" src="../images/logo-2.png"></a>
+            <a href="login.html" class="login">注销</a>
+            <a href="#" class="register"><? echo $name?></a>
+        </div>
+        <div class="index-header-slogan box">
+            <img src="../images/slogan.png">
+        </div>
+        <div class="index-header-search box">
+            <input type="text" placeholder="输入你的向往之地……">
+        </div>
+    </div>
+</div>
+<div class="index-display-panel box">
+    <div class="big-title"><img src="../images/当季热门.png" alt="当季热门"></div>
+    <div class="sub-title">
+        <ul>
+            <li><a href="#" class="city-active">广州</a></li>
+            <li><a href="#">上海</a></li>
+            <li><a href="#">石家庄</a></li>
+            <li><a href="#">北京</a></li>
+        </ul>
+    </div>
+    <div class="hotel-display">
+        <div id="dialog_large_image"></div>
+        <li>
+            <div><img class="apreview" src="../images/gz01.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家广州南沙广场店</p></a>
+            <p class="hotel-price">人均￥180/晚</p>
+        </li>
+        <li>
+            <div><img class="apreview" src="../images/gz02.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家广州番禺长隆北门店</p></a>
+            <p class="hotel-price">人均￥288/晚</p>
+        </li>
+        <li>
+            <div><img class="apreview" src="../images/gz03.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家广州天河沙河服装城店</p></a>
+            <p class="hotel-price">人均￥340/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/gz04.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家黄浦东区时代城店</p></a>
+            <p class="hotel-price">人均￥220/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/gz05.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家广州盘福路越秀公园店</p></a>
+            <p class="hotel-price">人均￥330/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/gz06.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家广州萝岗香雪地铁站店</p></a>
+            <p class="hotel-price">人均￥178/晚</p>
+        </li>
+    </div>
+</div>
+<div class="index-sub-banner">
+    <div class="index-sub-banner-cover"></div>
+    <div class="index-sub-banner-text box">
+        <img src="../images/特别推出莹家夏日度假村.png">
+    </div>
+    <div class="index-sub-banner-button box">
+        <a href="">浏览莹家夏季精选</a>
+    </div>
+</div>
+<div class="index-display-panel box">
+    <div class="big-title"><img src="../images/高分收藏.png" alt="高分收藏"></div>
+    <div class="sub-title"></div>
+    <div class="hotel-display">
+        <li>
+            <div><img src="../images/favourite01.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家三亚红糖湾建国度假酒店</p></a>
+            <p class="hotel-price">人均￥310/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/favourite02.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家三亚南山休闲会馆</p></a>
+            <p class="hotel-price">人均￥598/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/favourite03.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家珠海海泉湾度假温泉酒店</p></a>
+            <p class="hotel-price">人均￥480/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/favourite04.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家亚欧大酒店</p></a>
+            <p class="hotel-price">人均￥660/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/favourite05.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家三亚美高梅度假酒店</p></a>
+            <p class="hotel-price">人均￥1399/晚</p>
+        </li>
+        <li>
+            <div><img src="../images/favourite06.jpg"></div>
+            <a href="#"><p class="hotel-name">莹家长白沙酒店</p></a>
+            <p class="hotel-price">人均￥2206/晚</p>
+        </li>
+    </div>
+</div>
+<div class="public-footer">
+    <div class="footer-content box">
+        <ul>
+            <li><a href="">关于我们&nbsp;|&nbsp;</a></li>
+            <li><a href="">联系我们&nbsp;|&nbsp;</a></li>
+            <li><a href="">意见反馈&nbsp;|&nbsp;</a></li>
+            <li><a href="">营业执照&nbsp;|&nbsp;</a></li>
+            <li><a href="">会员中心&nbsp;|&nbsp;</a></li>
+            <li><a href="">指南</a></li>
+        </ul>
+        <br>
+        <p>© 2018 莹家酒店 yingjia.com 版权所有</p>
+    </div>
+</div>
+</body>
+</html>
---
+++ html/login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(!isset($_SESSION['user_id'])){
+	$con = mysql_connect("localhost","root","password") or die('Cloud not connect:'.mysql_error());
+	mysql_select_db("test",$con);
+	$un = $_POST['username'];
+	$pwd = $_POST['password'];
+	$result=mysql_query("SELECT * from user where username=$un");
+	$row = mysql_fetch_array($result);
+	if($row) {
+		if($row['password'] == $pwd) {
+			$_SESSION['username']=$un;
+			echo "<script>alert('登录成功！');window.location.href='loged.php'</script>";
+		} else {
+			echo "<script>alert('密码错误！');window.location.href='login.html'</script>";
+		}
+	} else {
+		echo "<script>alert('用户名不存在');window.location.href='login.html'</script>";
+	}
+}else{//如果用户已经登录，则直接跳转到已经登录页面
+    $home_url = 'loged.php';
+    header('Location: '.$home_url);
+}
+
+
+?>
